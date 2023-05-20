88d3e451077a4982fe67ea902287a094d8ebc337
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-05-20 16:24:22 +0700

    Create No_7_ReverseInteger.py

---
+++ No_7_ReverseInteger.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def reverse(self, x):
+        sign = 1
+        if x<0: 
+            sign = -1
+            x = abs(x)
+        while not x%10 and x:
+            x /= 10 
+        ans = int(str(x)[::-1]) * sign
+        return 0 if ans> pow(2, 31)-1 or ans<pow(-2, 31) else ans
