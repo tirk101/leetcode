ecfdea4ae9f8a60834924d4bb7b31726dc28754c
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-05-20 16:10:31 +0700

    Create No_6_ ZigzagConversion.py

---
+++ No_6_ ZigzagConversion.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        lst = ["" for i in range(numRows)]
+        idx = 0
+        step = 1
+        for i in range(len(s)):
+            lst[idx] += s[i]
+            if idx + step >= numRows : step = -1
+            elif idx + step < 0: step = 1 
+            idx += step
+        ans = ""
+        for i in lst: ans += i
+        return ans
