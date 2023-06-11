a0bb3daa1b2b59c4170306dbed2752eb445d0bde
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-06-12 03:19:10 +0700

    Create No_10_RegularExpressionMatching.py

---
+++ No_10_RegularExpressionMatching.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        memoize = {}
+        def recursive(i, j):
+            if (i, j) in memoize: return memoize[(i, j)]
+            if i >= len(s) and j >= len(p): return True
+            if j >= len(p): return False
+
+            check = True if i < len(s) and (s[i] == p[j] or p[j] == '.') else False
+            if j+1 < len(p) and p[j+1] == '*': 
+                memoize[(i, j)] = recursive(i, j+2) or (check and recursive(i+1, j))
+                return memoize[(i, j)]
+            if check: 
+                memoize[(i, j)] = recursive(i+1, j+1)
+                return memoize[(i, j)] 
+            memoize[(i, j)] = False
+            return False
+
+        return recursive(0, 0)
