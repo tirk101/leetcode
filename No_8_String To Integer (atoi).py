89b39fef2383b3ac68ded12ad8d0d52db5971d52
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-08-29 21:33:50 +0700

    Update No_8_String To Integer (atoi).py

--- No_8_String To Integer (atoi).py
+++ No_8_String To Integer (atoi).py
@@ -1,5 +1,9 @@
 class Solution(object):
     def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         sign = 1
         nums = ['0','1','2','3','4','5','6','7','8','9']
         temp = ""
