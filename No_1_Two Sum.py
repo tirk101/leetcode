66ecbf5faa0c68c6a9d820e2fdeedfa481a65179
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-08-29 21:37:09 +0700

    Update No_1_Two Sum.py

--- No_1_Two Sum.py
+++ No_1_Two Sum.py
@@ -1,5 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         ans = []
         for i in range(len(nums)):
             for j in range(i+1, len(nums)) :
