61a54f1a30e22d34dd8430ac8cc2743b598e2ece
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-08-29 21:34:42 +0700

    Update No_5_ Longest Palindromic Substring.py

--- No_5_ Longest Palindromic Substring.py
+++ No_5_ Longest Palindromic Substring.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         for i in range(len(s), 0, -1):
             for j in range(len(s)-i+1):
                 ans = s[j:i+j]
