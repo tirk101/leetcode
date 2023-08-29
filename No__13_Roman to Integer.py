8c3d5ebec86b18c0c9635714658bb17ca71ce87d
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-08-29 21:32:18 +0700

    Update No__13_Roman to Integer.py

--- No__13_Roman to Integer.py
+++ No__13_Roman to Integer.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         roman_str = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
         replace_str = {'IV':'I'*4, 'IX':'V'+'I'*4, 'XL':'X'*4, 'XC':'L'+'X'*4, 'CD':'C'*4, 'CM':'D'+'C'*4}
         for i in replace_str.keys(): s = s.replace(i, replace_str[i])
