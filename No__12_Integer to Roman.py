7e74a4eda159642ba8335dae946ad07064b07ad8
Krit Tangpinyoputtikhun <88623313+tirk101@users.noreply.github.com>
2023-08-29 21:32:43 +0700

    Update No__12_Integer to Roman.py

--- No__12_Integer to Roman.py
+++ No__12_Integer to Roman.py
@@ -1,5 +1,9 @@
 class Solution(object):
     def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
         roman = ""
         roman_str = {1:'I', 4:'IV', 5:'V', 9:'IX', 10:'X', 40:'XL', 50:'L', 
                      90:'XC', 100:'C', 400:'CD', 500:'D', 900:'CM',1000:'M'}
